06819e5f760d8ef50c3053d7cc497edc0951b2a1
Gass-Dev <madebygass@gmail.com>
2021-06-14 00:40:57 +0200

    WIP postReports

--- dump/src/components/pages/PostReport/Post_Report.jsx
+++ dump/src/components/pages/PostReport/Post_Report.jsx
@@ -1,94 +1,163 @@
 // Imports
-import React, { useState } from 'react';
-import Axios from 'axios';
-import { useHistory } from 'react-router-dom';
-import { useAlert } from 'react-alert';
+import React, { useState } from "react";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
+import { useAlert } from "react-alert";
 
-require('./_postReport.scss');
+require("./_postReport.scss");
 
 function PostRegister(props) {
-    const history = useHistory();
-    const userId = localStorage.getItem('userId');
-    const [post, setPost] = useState(
-        {numberStreet: '', street: '', postalCode: '', city: '', report: '', userId }
-    );
+  const history = useHistory();
+  const userId = localStorage.getItem("userId");
+  const [post, setPost] = useState({
+    numberStreet: "",
+    street: "",
+    postalCode: "",
+    city: "",
+    report: "",
+    userId,
+  });
 
-    const [errorForm, setErrorForm] = useState(" ")
+  const [errorForm, setErrorForm] = useState(" ");
 
-    const alert = useAlert()
+  const alert = useAlert();
 
-    const handleChange = (event) => {
-        setPost({ ...post, [event.target.name]: event.target.value })
-    }
+  const handleChange = (event) => {
+    setPost({ ...post, [event.target.name]: event.target.value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        Axios.post('http://localhost:8000/api/post_reports/post', post)
-            .then((response) => {
-                setPost({numberStreet: '', street: '', postalCode: '', city: '', report: ''})
-                alert.show('Signalement enregistré 😊')
-                history.push('/')
-            })
-            .catch((error) => {
-                setErrorForm(error.response.data.error)
-            })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:8000/api/post_reports/post", post)
+      .then((response) => {
+        setPost({
+          numberStreet: "",
+          street: "",
+          postalCode: "",
+          city: "",
+          report: "",
+        });
+        alert.show("Signalement enregistré 😊");
+        history.push("/");
+      })
+      .catch((error) => {
+        setErrorForm(error.response.data.error);
+      });
+  };
 
-    return (
-        <form className="postRegisterForm" method="POST" action="/post_reports/post" onSubmit={handleSubmit}>
+  return (
+    <form
+      className="postRegisterForm"
+      method="POST"
+      action="/post_reports/post"
+      onSubmit={handleSubmit}
+    >
+      <div className="postRegisterForm_content">
+        <div className="postRegisterForm_content_title">
+          <h2>Signalement</h2>
+        </div>
 
-            <div className="postRegisterForm_content">
-                <div className="postRegisterForm_content_title">
-                    <h2>Signalement</h2>
-                </div>
+        <div className="postRegisterForm_content_type">
+          <p>Quel type de signalement souhaites-tu faire?</p>
 
-                <div className="postRegisterForm_content_type">
-                    <p>Quel type de signalement souhaites-tu faire?</p>
-                    
-                    <div className="postRegisterForm_content_type_encombrant">
-                        <label htmlFor="reportEncombrant">Encombrant</label>
-                        <input type="radio" id="reportEncombrant" value="reportEncombrant" name='report' onChange={handleChange} required />
-                    </div>
+          <div className="postRegisterForm_content_type_encombrant">
+            <label htmlFor="reportEncombrant">Encombrant</label>
+            <input
+              type="radio"
+              id="reportEncombrant"
+              value="reportEncombrant"
+              name="report"
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                    <div className="postRegisterForm_content_type_dechet">    
-                        <label htmlFor="reportDechet">Déchet     </label>
-                        <input type="radio" id="reportDechet" value="reportDechet" name='report' onChange={handleChange} required />
-                    </div>
-                        
-                    <div className="postRegisterForm_content_type_insalubrite">
-                        <label htmlFor="reportInsalubrite">Insalubrité</label>
-                        <input type="radio" id="reportInsalubrite" value="reportInsalubrite" name='report' onChange={handleChange} required />
-                    </div>
+          <div className="postRegisterForm_content_type_dechet">
+            <label htmlFor="reportDechet">Déchet </label>
+            <input
+              type="radio"
+              id="reportDechet"
+              value="reportDechet"
+              name="report"
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                </div>
+          <div className="postRegisterForm_content_type_insalubrite">
+            <label htmlFor="reportInsalubrite">Insalubrité</label>
+            <input
+              type="radio"
+              id="reportInsalubrite"
+              value="reportInsalubrite"
+              name="report"
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                <div className="postRegisterForm_content_adress">
-                    <p>Où est ce signalement?</p>
+        <div className="postRegisterForm_content_adress">
+          <p>Où est ce signalement?</p>
 
-                    <p>N° de voie</p>
-                    <input type="text" id="numberStreetRegister" name="numberStreet" placeholder="1" value={post.numberStreet} onChange={handleChange} required />
+          <p>N° de voie</p>
+          <input
+            type="text"
+            id="numberStreetRegister"
+            name="numberStreet"
+            placeholder="1"
+            value={post.numberStreet}
+            onChange={handleChange}
+            required
+          />
 
-                    <p>Nom de la rue</p>
-                    <input type="street" id="streetRegister" name="street" placeholder="rue Sébastopole" value={post.street} onChange={handleChange} required />
+          <p>Nom de la rue</p>
+          <input
+            type="street"
+            id="streetRegister"
+            name="street"
+            placeholder="rue Sébastopole"
+            value={post.street}
+            onChange={handleChange}
+            required
+          />
 
-                    <p>Code Postal</p>
-                    <input type="text" id="postalcodeRegister" name="postalCode" placeholder="75001" value={post.postalCode} onChange={handleChange} required />
+          <p>Code Postal</p>
+          <input
+            type="text"
+            id="postalcodeRegister"
+            name="postalCode"
+            placeholder="75001"
+            value={post.postalCode}
+            onChange={handleChange}
+            required
+          />
 
-                    <p>Ville</p>
-                    <input type="city" id="cityRegister" name="city" placeholder="Paris" value={post.city} onChange={handleChange} required />
+          <p>Ville</p>
+          <input
+            type="city"
+            id="cityRegister"
+            name="city"
+            placeholder="Paris"
+            value={post.city}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-                </div>
+        <div className="postRegisterForm_content_submit">
+          <button
+            className="postRegisterForm_content_submit_button"
+            type="submit"
+          >
+            Valider
+          </button>
+        </div>
 
-                <div className="postRegisterForm_content_submit">
-                    <button className="postRegisterForm_content_submit_button" type="submit">Valider</button>
-                </div>
-                
-                <div>
-                    {errorForm}
-                </div>
-            </div>
-        </form>
-    )
+        <div>{errorForm}</div>
+      </div>
+    </form>
+  );
 }
 
 export default PostRegister;
--- dump/src/components/pages/Register/Register.jsx
+++ dump/src/components/pages/Register/Register.jsx
@@ -1,101 +1,195 @@
 // Imports
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import Axios from 'axios';
-import { useAlert } from 'react-alert';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import Axios from "axios";
+import { useAlert } from "react-alert";
 
-import Logo from '../../../assets/images/logo_dump.png';
+import Logo from "../../../assets/images/logo_dump.png";
 
-require('./_register.scss');
+require("./_register.scss");
 
 function Register(props) {
-    const history = useHistory();
-    const [register, setRegister] = useState(
-        { userName: '', email: '', password: '', firstName: '', lastName: '', numberStreet: '', street: '', postalCode: '', city: '' }
-    );
-    const [errorForm, setErrorForm] = useState(" ")
-    const alert = useAlert()
+  const history = useHistory();
+  const [register, setRegister] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    numberStreet: "",
+    street: "",
+    postalCode: "",
+    city: "",
+  });
+  const [errorForm, setErrorForm] = useState(" ");
+  const alert = useAlert();
 
-    const handleChange = (event) => {
-        setRegister({ ...register, [event.target.name]: event.target.value })
-    }
+  const handleChange = (event) => {
+    setRegister({ ...register, [event.target.name]: event.target.value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        Axios.post('http://localhost:8000/api/users/register', register)
-            .then((response) => {
-                setRegister({ userName: '', email: '', password: '', firstName: '', lastName: '', numberStreet: '', street: '', postalCode: '', city: '' })
-                alert.show('Inscription validée!')
-                history.push('/')
-            })
-            .catch((error) => {
-                setErrorForm(error.response.data.error)
-            })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:8000/api/users/register", register)
+      .then((response) => {
+        setRegister({
+          userName: "",
+          email: "",
+          password: "",
+          firstName: "",
+          lastName: "",
+          numberStreet: "",
+          street: "",
+          postalCode: "",
+          city: "",
+        });
+        alert.show("Inscription validée!");
+        history.push("/");
+      })
+      .catch((error) => {
+        setErrorForm(error.response.data.error);
+      });
+  };
 
-    return (
-        <form className="registerForm" method="POST" action="/register" onSubmit={handleSubmit}>
+  return (
+    <form
+      className="registerForm"
+      method="POST"
+      action="/register"
+      onSubmit={handleSubmit}
+    >
+      <div className="registerForm_logo">
+        <Link to="/" className="registerForm_logo_link">
+          <img className="registerForm_logo_link" src={Logo} alt="logo" />
+        </Link>
+      </div>
 
-            <div className="registerForm_logo">
-                <Link to='/' className="registerForm_logo_link">
-                    <img className="registerForm_logo_link" src={Logo} alt='logo' />
-                </Link>
-            </div>
+      <div className="registerForm_title">
+        <h2>Inscription</h2>
+      </div>
 
-            <div className="registerForm_title">
-                <h2>Inscription</h2>
-            </div>
+      <div className="registerForm_userName">
+        <p>Entre ton pseudo</p>
+        <input
+          type="text"
+          name="userName"
+          id="userName"
+          placeholder="Dumpeuse75"
+          value={register.userName}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
-            <div className="registerForm_userName">
-                <p>Entre ton pseudo</p>
-                <input type="text" name="userName" id="userName" placeholder="Dumpeuse75" value={register.userName} onChange={handleChange} required />
-            </div>
+      <div className="registerForm_email">
+        <p>Entre ton email</p>
+        <input
+          type="email"
+          name="email"
+          id="emailRegister"
+          placeholder="dump@mail.com"
+          value={register.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
-            <div className="registerForm_email">
-                <p>Entre ton email</p>
-                <input type="email" name="email" id="emailRegister" placeholder="dump@mail.com" value={register.email} onChange={handleChange} required />
-            </div>
+      <div className="registerForm_password">
+        <p>Quel mot de passe as tu choisis?</p>
+        <input
+          type="password"
+          id="passRegister"
+          name="password"
+          placeholder="Mot de passe"
+          value={register.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
-            <div className="registerForm_password">
-                <p>Quel mot de passe as tu choisis?</p>
-                <input type="password" id="passRegister" name="password" placeholder="Mot de passe" value={register.password} onChange={handleChange} required />
-            </div>
+      <div className="registerForm_firstName">
+        <p>Entre ton prénom</p>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          placeholder="Marie"
+          value={register.firstName}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
-            <div className="registerForm_firstName">
-                <p>Entre ton prénom</p>
-                <input type="text" name="firstName" id="firstName" placeholder="Marie" value={register.firstName} onChange={handleChange} required />
-            </div>
+      <div className="registerForm_lastName">
+        <p>Entre ton nom de famille</p>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          placeholder="Dupont"
+          value={register.lastName}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
-            <div className="registerForm_lastName">
-                <p>Entre ton nom de famille</p>
-                <input type="text" name="lastName" id="lastName" placeholder="Dupont" value={register.lastName} onChange={handleChange} required />
-            </div>
+      <div className="registerForm_adress">
+        <p>Où habites-tu?</p>
 
-            <div className="registerForm_adress">
-                <p>Où habites-tu?</p>
+        <p>N° de voie</p>
+        <input
+          type="text"
+          id="numberStreetRegister"
+          name="numberStreet"
+          placeholder="1"
+          value={register.numberStreet}
+          onChange={handleChange}
+          required
+        />
 
-                <p>N° de voie</p>
-                <input type="text" id="numberStreetRegister" name="numberStreet" placeholder="1" value={register.numberStreet} onChange={handleChange} required />
+        <p>Nom de la rue</p>
+        <input
+          type="text"
+          id="streetRegister"
+          name="street"
+          placeholder="rue Sébastopole"
+          value={register.street}
+          onChange={handleChange}
+          required
+        />
 
-                <p>Nom de la rue</p>
-                <input type="text" id="streetRegister" name="street" placeholder="rue Sébastopole" value={register.street} onChange={handleChange} required />
+        <p>Code Postal</p>
+        <input
+          type="text"
+          id="postalCodeRegister"
+          name="postalCode"
+          placeholder="75001"
+          value={register.postalCode}
+          onChange={handleChange}
+          required
+        />
 
-                <p>Code Postal</p>
-                <input type="text" id="postalCodeRegister" name="postalCode" placeholder="75001" value={register.postalCode} onChange={handleChange} required />
+        <p>Ville</p>
+        <input
+          type="text"
+          id="cityRegister"
+          name="city"
+          placeholder="Paris"
+          value={register.city}
+          onChange={handleChange}
+          required
+        />
+      </div>
 
-                <p>Ville</p>
-                <input type="text" id="cityRegister" name="city" placeholder="Paris" value={register.city} onChange={handleChange} required />
-            </div>
+      <div className="registerForm_submit">
+        <button className="registerForm_submit_button" type="submit">
+          Envoyer
+        </button>
+      </div>
 
-            <div className="registerForm_submit">
-                <button className="registerForm_submit_button" type="submit">Envoyer</button>
-            </div>
-
-            <div>
-                {errorForm}
-            </div>
-        </form>
-    )
+      <div>{errorForm}</div>
+    </form>
+  );
 }
 
 export default Register;
